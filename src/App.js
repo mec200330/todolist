4adc5228edca1e81defa485aac7bdf93491c5a00
Cristhian <cristhiangomezdev@gmail.com>
2023-03-19 11:52:33 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import Container from './Components/Container'
+import Header from './Components/Header';
+import InputTask from './Components/InputTask';
+import { useEffect, useState } from 'react';
+import TaskContent from './Components/TaskContent';
+
 
 function App() {
+//pasar al localstorage
+let initialTask = JSON.parse(localStorage.getItem("tasks"))
+
+if(!initialTask){
+  initialTask=[]
+}
+
+const [tasks, setTasks] = useState(initialTask);
+
+
+useEffect(()=>{
+if(initialTask){
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+}else{
+  localStorage.setItem("tasks", JSON.stringify([]))
+}
+}, [initialTask, tasks])
+
+
+const createTask =(task)=>{
+console.log(task);
+setTasks([...tasks, task])
+}
+
+const deleteTask =(id)=>{
+
+const currentTask = tasks.filter((task)=>task.idTask !== id)
+setTasks(currentTask)
+
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+    <Header/>
+    <InputTask createTask={createTask} />
+    <TaskContent  tasks={tasks} deleteTask={deleteTask}/>
+   </Container>
   );
 }
 
---
+++ src/Components/InputTask.js
@@ -0,0 +1,92 @@
+import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
+import { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
+
+const options =[
+{key: "deporte", text:"Deporte", value:"Deporte"},
+{key: "casa", text:"Casa", value:"casa"},
+{key: "oficina", text:"Oficina", value:"Oficina"},
+{key: "otra", text:"Otra", value:"Otra"}
+]
+
+
+export default function InputTask(props) {
+
+const [task, setTask] = useState({
+    idTask:"",
+    taskName:"",
+    categoryTask:""
+});
+
+const [error, setError] = useState(false)
+const {createTask} = props;
+
+const onChangeTask =(e)=>{
+    setTask({...task, [e.target.name]: e.target.value })
+}
+
+const onChangeCategoryTask =(e, data)=>{
+    setTask({...task, [data.name]:data.value })
+    console.log(task);
+}
+
+
+const onSubmitTask =(e)=>{
+    
+    e.preventDefault() //no recarga l apagina
+
+    if(task.taskName.trim()==="" || task.categoryTask.trim()===""){
+        setError(true)
+        return;
+    }
+//Eliminar mensaje de error
+setError(false)
+//Asignar un id
+task.idTask = uuidv4()
+console.log(uuidv4());
+//Crear la tarea
+createTask(task)
+
+//Limpiando el form
+setTask({idTask:"",
+taskName:"",
+categoryTask:""})
+
+}
+
+
+  return (
+    <>
+      <Grid centered columns={2}>
+        <Input type="text" action>
+          <Input
+            size="small"
+            icon="add"
+            placeholder="Escribe tu tarea"
+            iconPosition="left"
+            name="taskName"
+            value={task.taskName}
+            onChange={onChangeTask}
+          />
+          <Select
+            compact
+            options={options}
+            className="select-form-task"
+            name="categoryTask"
+            placeholder="Categoria"
+            value={task.categoryTask}
+            onChange={onChangeCategoryTask}
+          />
+          <Button type="submit" color="teal" onClick={onSubmitTask}>Añadir tarea</Button>
+        </Input>
+      </Grid>
+      {error && (<Grid centered>
+        <Header as='h4' color="red" className="alert-error-form">
+            <Icon name="close"/>
+            <Header.Content>No se puede dejar tarea ni categoría en blanco</Header.Content>
+            <Icon name="close"/>
+        </Header>
+      </Grid>)}
+    </>
+  );
+}
